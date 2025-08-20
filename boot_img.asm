735da30dbe940da36e080a76c798072b1464cd17
PythonCoders45 <gstar3181@gmail.com>
2025-08-20 12:46:54 -0400

    Update boot_img.asm

--- boot_img.asm
+++ boot_img.asm
@@ -1,4 +1,4 @@
-; boot_img.asm — Bootloader that loads 320x200 image from disk
+; boot_img.asm — Minimal graphical bootloader with 320x200 mode
 BITS 16
 ORG 0x7C00
 
@@ -14,37 +14,38 @@ start:
     mov ax, 0x0013
     int 0x10
 
-    ; Load image from disk (320*200 = 64000 bytes => 125 sectors)
-    mov bx, 0x0000       ; VGA memory offset
+    ; Clear screen (color 0 = black)
+    xor di, di
+    mov cx, 320*200
+    mov al, 0
+    mov es, 0xA000
+    rep stosb
+
+    ; Load kernel (assume sector 2 onward, 8 sectors for demo)
+    mov bx, 0x0000       ; VGA memory offset (not used)
     mov si, 2            ; first sector after bootloader
-    mov cx, 125          ; number of sectors to read
+    mov cx, 8            ; number of sectors to read
 .load_loop:
     push cx
-    mov ah, 0x02         ; BIOS read sectors
-    mov al, 1            ; read 1 sector
-    mov ch, 0            ; cylinder
-    mov dh, 0            ; head
-    mov dl, 0x80         ; first HDD
-    mov cl, si           ; sector number
-    mov es, 0xA000
-    mov di, bx
+    mov ah, 0x02
+    mov al, 1
+    mov ch, 0
+    mov dh, 0
+    mov dl, 0x80
+    mov cl, si
+    mov es, 0x0000
+    mov di, 0x8000
     int 0x13
     jc .disk_error
-    add bx, 512
     inc si
     pop cx
     loop .load_loop
 
-    ; Wait a key press
-    xor ah, ah
-    int 0x16
-
-    ; Jump to kernel at 0x8000
+    ; Jump to kernel at 0x0000:0x8000
     jmp 0x0000:0x8000
 
 .disk_error:
-    ; Print simple error
-    mov si, diskerr
+    mov si, err
 .print_char:
     lodsb
     cmp al, 0
@@ -56,7 +57,7 @@ start:
     hlt
     jmp .hang
 
-diskerr db "Disk read error!",0
+err db "Disk read error!",0
 
 ; Pad to 512 bytes
 times 510-($-$$) db 0
